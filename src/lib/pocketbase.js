e2df21d8e02e11ba37305d9e86563657b3821234
SuvarnaMandadi <mandadisuvarna2001@gmail.com>
2025-07-28 14:08:58 -0500

    Fix: point PocketBase to Render backend

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase('https://pocketbase-deploy-kkjz.onrender.com');
 
 if (typeof localStorage !== 'undefined') {
 	const auth = localStorage.getItem('pb_auth');
