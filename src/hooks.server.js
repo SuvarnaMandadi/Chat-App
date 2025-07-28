33948ef100fb5f8dd5d7dfc3ba75ebeb27fb38ad
SuvarnaMandadi <mandadisuvarna2001@gmail.com>
2025-07-28 13:14:08 -0500

    Update PocketBase URL

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,16 +1,16 @@
 import PocketBase from 'pocketbase';
 
 export async function handle({ event, resolve }) {
-	const pb = new PocketBase('http://127.0.0.1:8090');
-	const cookie = event.request.headers.get('cookie') || '';
+	const pb = new PocketBase('https://pocketbase-deploy-kkjz.onrender.com'); // ✅ LIVE URL
 
+	const cookie = event.request.headers.get('cookie') || '';
 	pb.authStore.loadFromCookie(cookie);
+
 	event.locals.pb = pb;
 	event.locals.user = pb.authStore.model;
 
 	const response = await resolve(event);
 
-	// sync cookie to browser
 	response.headers.set(
 		'set-cookie',
 		pb.authStore.exportToCookie({ httpOnly: false, path: '/', maxAge: 60 * 60 * 24 * 7 })
